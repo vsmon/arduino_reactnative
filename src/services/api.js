d1b9132bb3a5cc4bf370e3442ff8ced47ee2ca08
Rodrigo <45691540+vsmon@users.noreply.github.com>
2020-03-07 21:21:19 -0300

    Update api.js

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import AsyncStorage from '@react-native-community/async-storage';
 
-let address = 'http://192.168.0.40:3001'; //'http://189.101.143.213:3001'
+let address = 'http://192.168.0.40:3001';
 
 async function getAddress() {
   const res = await AsyncStorage.getItem('address');
