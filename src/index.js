46dd397bb621eb8ae07877fd2e291c620f6417fc
Rodrigo <rodrigo.fonsecamartins@gmail.com>
2021-10-17 02:35:01 -0300

    added notification

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import {StatusBar} from 'react-native';
 
 import Routes from './routes';
+import './config/NotificationConfig';
 
 export default function App() {
   return (
